1d9b16afce95253e811d6ac9209b9da6f219d240
keith-kay <keithrhova@gmail.com>
2022-12-09 03:01:51 -0800

    pritnf.c

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main function - Prints "with proper grammar, but the outcome is a 
+ * 			piece of art, "
+ * return 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
+}
