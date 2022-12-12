34b5081fbe5332e133093fb549649545a71d9b2b
keith-kay <keithrhova@gmail.com>
2022-12-12 04:48:32 -0800

    3-prints_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,9 @@ int main(void)
 	/* print lowercase letters */
 	for(ch = 'a'; ch <= 'z'; ch++)
 	putchar(ch);
-	putchar('\n');
-	return (0);
 	/* prints uppercase letters */
 	for (ch = 'A'; ch <= 'B'; ch++)
 	putchar(ch);
-	putchar('\n');
+	putchar('\n')
 	return (0);
 }
