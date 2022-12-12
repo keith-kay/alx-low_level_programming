a0cd9d7d5ad0ec7ef6f3e6dacb85225b9a3f79cf
keith-kay <keithrhova@gmail.com>
2022-12-12 06:48:54 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,7 +5,7 @@
 #include <time.h>
 /**
  * main - prints single digit numbers
- * Return - Always (Success)
+ * Return:  Always (Success)
  *
  */
 int main(void)
@@ -15,6 +15,5 @@ int main(void)
 	for (x = 0; x < 10; x++)
 		putchar((x % 10) + '0');
 	putchar('\n');
-
 	return (0);	
 }
