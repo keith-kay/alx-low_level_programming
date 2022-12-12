7ff981609935be5ed80aa042ab6ccf87239d03a4
keith-kay <keithrhova@gmail.com>
2022-12-12 12:18:51 -0800

    7-print_tebaplah.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 #include <time.h>
 
+/**
+ * main - prints alphabets in reverse
+ * Return: Always (Success)
+ *
+ */
 int main(void)
 {
 	char ch;
 
 	for (ch = 'z'; ch <= 'a'; ch--)
 		putchar(ch);
+	putchar('\n');
 
 	return (0);
 }
