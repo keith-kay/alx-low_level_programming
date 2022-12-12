4bcea4e51477455983a2e476ddc0234ba49c9227
keith-kay <keithrhova@gmail.com>
2022-12-12 12:40:32 -0800

    8-print_base_16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 	int num;
 	char ch;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
