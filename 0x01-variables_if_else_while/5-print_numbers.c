d3090a5c996ffa1cc5b0a4df7fff97a06bd8ee14
keith-kay <keithrhova@gmail.com>
2022-12-12 05:32:08 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Prints single digit numbers of base 10
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	printf("%d", x);
+	printf("\n");
+
+	return (0);
+	
+}
