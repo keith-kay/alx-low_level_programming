7f32b70c1f7fbc2937837c7f38ef01a414b5d97c
keith-kay <keithrhova@gmail.com>
2022-12-14 05:41:37 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,17 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
+	int i = 0;
 	char x;
-
-	while (i < 10)
+	
+	while (i <= 9)
 	{
 		for (x = 'a'; x <= 'z'; x++)
 		{
-		_putchar(x);
+			_putchar(x);
+		}
 		_putchar('\n');
 		i++;
-		}
 	}
+
 }
