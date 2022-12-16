470b73d88ecf6bfc3ed0c7908c46a381cf7a222c
keith-kay <keithrhova@gmail.com>
2022-12-16 02:32:16 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,15 +10,16 @@ void print_line(int n)
 {
 	int x;
 
-	if (n <= 0)	{
-	_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
 	}
 	else 
 	{
-	for (x = 0; x < n; x++)
+		for (x = 0; x < n; x++)
 	{
-	_putchar(95);
+		_putchar(95);
 	}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
