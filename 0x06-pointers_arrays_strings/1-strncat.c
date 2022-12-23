4537bd9b15ec6e98044674d1a416b70e6824ed56
keith-kay <keithrhova@gmail.com>
2022-12-22 19:03:57 -0800

    Task 1 - 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,21 +8,22 @@
 * @n: input value
 *Return: dest
 */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int x;
 	int y;
-
+	
 	x = 0;
 	while (dest[x] != '\0')
 	{
 		x++;
 	}
 	while (y < n && src[y] != '\0')
-	{
-	dest[x] = src[y];
-	x++;
-	y++;
+	{	
+		dest[x] = src[y];
+		x++;
+		y++;
 	}
 	dest[x] = '\0';
 	return (dest);
