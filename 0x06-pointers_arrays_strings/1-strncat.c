41ae744382ef3ad5e53f26bb5fd270f4ae8f86f0
keith-kay <keithrhova@gmail.com>
2022-12-22 19:01:02 -0800

    Task 1 - 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*_strncat - concantenate two strings
+* using atmost n-bytes from src
+* @dest: input value
+* @src: input value
+* @n: input value
+*Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (y < n && src[y] != '\0')
+	{
+	dest[x] = src[y];
+	x++;
+	y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
