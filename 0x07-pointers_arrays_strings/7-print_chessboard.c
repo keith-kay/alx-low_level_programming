42a8ed77b075ff0533a32f88397a40cc79a63b2d
keith-kay <keithrhova@gmail.com>
2023-01-03 14:10:59 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i < 0; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		for (j =0; j < 0; j++)
+		for (j = 0; j <= 7; j++)
+		{
 			_putchar(a[i][j]);
-		_putchar('\n');
+		}
+		_putchar(10);
 	}
 }
