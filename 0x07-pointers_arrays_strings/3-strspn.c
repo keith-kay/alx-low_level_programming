0aba6b25e2fcbc688ba7a46cc44402aa0f2b67a1
keith-kay <keithrhova@gmail.com>
2023-01-03 05:34:20 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,18 +9,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
-	int x;
+	int r;
 
 	while (*s)
 	{
-		for (x = 0; accept[x]; x++)
+		for (r = 0; accept[r]; r++)
 		{
-			if (*s == accept[x])
+			if (*s == accept[r])
 			{
-				x++;
+				r++;
 				break;
 			}
-			else if (accept[x + 1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;
