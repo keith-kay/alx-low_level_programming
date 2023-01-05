112ca1d32de9023780b4a7fcd681f6a73e3d759b
keith-kay <keithrhova@gmail.com>
2023-01-05 13:28:35 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * all prototypes to be used in this task are 
+ * listed below
+ */
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char c);
