8c8943200f58f2bc0c0e776cb2d942fcd81b0cfa
keith-kay <keithrhova@gmail.com>
2023-01-05 16:48:25 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,11 +5,11 @@
  *
  * Return: The length of the string
  */
-int _strlen_recursion( char *s)
+int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if(*s > '\0')
+	if (*s > '\0')
 	{
 		i += _strlen_recursion(s + 1) + 1;
 	}
