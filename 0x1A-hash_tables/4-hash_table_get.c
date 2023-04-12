de23a5e8bee976a2c0ef657946cf37e0d1a295f2
keith-kay <keithrhova@gmail.com>
2023-04-12 10:48:20 -0700

    task 4 commit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,26 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieves a valuefrom the hashtable.
- * @ht: Pointer to the hash table.
- * @key: The key to look up in the hash table.
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: Hash table.
+ * @key: Key to look up in the hash table.
  *
- * Return: The value associated with the key if true, or NULL if key is not found.
+ * Return: The value if found, NULL if not.
  */
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
-
-	while (node != NULL && strcmp(node->key, key) != 0)
+	if (node == NULL)
+		return (NULL);
+	while (strcmp(node->key, key) && node != NULL)
+	{
 		node = node->next;
-
-	return (node == NULL ? NULL : node->value);
+	}
+	if (node == NULL)
+		return (NULL);
+	else
+		return (node->value);
 }
